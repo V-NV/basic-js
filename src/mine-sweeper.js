7665895cc02b1aeac1cd2f7c51618f3d2a9c3687
V-NV <96265492+V-NV@users.noreply.github.com>
2022-05-03 03:43:24 +0500

    fix:'lastSol'

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -22,10 +22,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [2, 1, 1],
  *  [1, 1, 1]
  * ]
- */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+ *//*****************решено********** */
+function minesweeper(matrix) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let set = [];
+  for(let i = 0; i < matrix.length; i++) {
+  set[i] = [];
+  for(let j = 0; j < matrix[0].length; j++) {
+      let n = 0;
+        if(matrix[i][j - 1]) n = n + 1;
+        if(matrix[i][j + 1]) n = n + 1;
+      if (matrix[i - 1]) {
+        if(matrix[i - 1][j]) n = n + 1;
+        if(matrix[i - 1][j + 1]) n = n + 1;
+        if(matrix[i - 1][j - 1]) n = n + 1;
+      }
+      if (matrix[i + 1]) {
+        if(matrix[i + 1][j]) n = n + 1;
+        if(matrix[i + 1][j + 1]) n = n + 1;
+        if(matrix[i + 1][j - 1]) n = n + 1;
+      }
+    set[i][j] = n;
+    }
+  }
+  return set;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -14,62 +14,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  //throw new NotImplementedError('Not implemented');
+   //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 //console.log(arr)
-  let arr2 = arr.slice().flat()//новый объект(массив)
+ // let arr2 = arr.slice().flat()//новый объект(массив)
 //console.log(arr)
 //let prev = '--discard-prev'.split();
 
-if(!Array.isArray(arr)) throw new Error('\'arr\' parameter must be an instance of the Array!');
-if(arr == 3){return arr}
-if(arr == 3.312312){return arr}
-if(arr == false){return arr}
-if(arr == null){return arr}
-if(arr == undefined){return arr}
-if(arr == { 'foo': 'bar' }){return arr}
-if(arr[0][0] == String){return [ 1, 2, 3 ]}
+if(!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+let arr2 = arr.slice().flat()//новый объект(массив)
+//if(arr == 3){return arr}
+//if(arr == 3.312312){return arr}
+//if(arr == false){return arr}
+//if(arr == null){return arr}
+//if(arr == undefined){return arr}
+//if(arr == { 'foo': 'bar' }){return arr}
+//if(arr[0][0]){return [ 1, 2, 3 ]}
 
 //if(arr[0] !== Number){return arr}
 
-for(i=0;i<arr.length;i++){
- if(arr2[i] == String && arr2[i-1] == undefined){return arr}
- //if(arr2[i] == String && arr2[i+1] == undefined){return arr}
 
-  
-         
-    if(arr2[i] == '--discard-next'){//удалить себя и +1
+for(i=0;i<arr2.length;i++){
+ 
+    if(arr2[i] == '--discard-next'){//удалить +1
   arr2.splice(`${i+1}`,1)
-  //arr2.splice(i,1)
-  console.log(arr2)
-  }
-
   
-  if(arr2[i] == '--discard-prev'){//удалить себя и -1ый
-    arr2.splice(i,1)
-    arr2.splice(`${i-1}`,1)
+  }
+  
+  if(arr2[i] == '--discard-prev'){//удалить -1ый
+    if(arr[i-1]){
+       arr2.splice(`${i-1}`,1)
+    }
 }
   if(arr2[i] == '--double-next'){//вместо себя +1ый
    delete arr2[i]
     arr2[i] = arr[i+1]
+    
 }
  
-  if(arr2[i] == '--double-prev'){//заменить себя -1ый
+  if(arr2[i] == '--double-prev'&&arr[i-1]){//заменить себя -1ый
   delete arr2[i]
     arr2[i] = arr[i-1]
+    
 }
-  
+}  
+   for(i=0;i<arr2.length;i++){
 if(arr2[i] == '--discard-next'){arr2.splice(i,1)} 
 if(arr2[i] == '--discard-prev'){arr2.splice(i,1)}
 if(arr2[i] == '--double-prev'){arr2.splice(i,1)}
 if(arr2[i] == '--double-next'){arr2.splice(i,1)}
 }
-console.log('')
-  console.log('')
+for(i=0;i<arr2.length;i++){
+if(arr2[i] == '--discard-next'){arr2.splice(i,1)} 
+if(arr2[i] == '--discard-prev'){arr2.splice(i,1)}
+if(arr2[i] == '--double-prev'){arr2.splice(i,1)}
+if(arr2[i] == '--double-next'){arr2.splice(i,1)}
+//if(arr2[i].incudes(undefined)){arr2.splice(i,1)}
+}
 
 return arr2;
+  
 
 }
+
 
 module.exports = {
   transform
